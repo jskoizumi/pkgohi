619748338587e0201f303b653ce9c04694653385
jskoizumi <jskoizumi@gmail.com>
2016-07-27 15:13:53 -1000

    Update __init__.py

--- pogom/__init__.py
+++ pogom/__init__.py
@@ -5,5 +5,5 @@ config = {
     'LOCALE': 'en',
     'LOCALES_DIR': 'static/locales',
     'ROOT_PATH': None,
-    'GOOGLEMAPS_KEY': 'GMAPS KEY HERE'
+    'GOOGLEMAPS_KEY': 'AIzaSyC4rtqls-Mv4UWF62WngyDSApCzORfHAU8'
 }
